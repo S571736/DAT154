d78db9d26f07f885ebe490f89385bbb0964515c9
S571736 <Sondre.Gjesdal@gmail.com>
2020-02-12 20:47:32 +0100

    Første del er ferdig, må fikse at man velger planet eller måne osv

--- Assignments/Assignment3/Mainprog.cs
+++ Assignments/Assignment3/Mainprog.cs
@@ -7,7 +7,7 @@ class Astronomy
     public static void Main()
     {
 
-        Star Sun = new Star("Sun");
+        Star Sun = new Star("Sun", 50, 0, "RED");
 
         Planet mercury = new Planet("Mercury", 3, 2, "ORANGE", 57910, 87, Sun);
         Planet venus = new Planet("Venus", 3, 2, "BROWN", 108200, 224, Sun);
@@ -17,18 +17,18 @@ class Astronomy
         Planet saturn = new Planet("Saturn", 10, 500, "BROWN", 1429400, 10759, Sun);
         Planet uranus = new Planet("Uranus", 10, 500, "GREEN", 2870990, 30685, Sun);
         Planet neptune = new Planet("Neptune", 10, 500, "BLUE", 4504300, 60190, Sun);
-        DwarfPlanet pluto = new Planet("Pluto", 10, 500, "GREY", 5913520, 90550, Sun);
+        DwarfPlanet pluto = new DwarfPlanet("Pluto", 10, 500, "GREY", 5913520, 90550, Sun);
 
 
         Moon moon = new Moon("Moon", 1, 0, "GREY", 384, 27, earth);
 
-        Moon phobis = new Moon("Phobos", 1, 0, "GREY", 9, 0, mars);
+        Moon phobis = new Moon("Phobos", 1, 0, "GREY", 9, 1, mars);
         Moon Deimos = new Moon("Deimos", 1, 0, "GREY", 23, 46023, mars);
 
-        Moon metis = new Moon("Metis", 1, 0, "GREY", 128, 0, jupiter);
-        Moon adrastea = new Moon("Adrastea", 1, 0, "GREY", 129, 0, jupiter);
-        Moon amalthea = new Moon("Amalthea", 1, 0, "GREY", 181, 0, jupiter);
-        Moon thebe = new Moon("Thebe", 1, 0, "GREY", 222, 0, jupiter);
+        Moon metis = new Moon("Metis", 1, 0, "GREY", 128, 1, jupiter);
+        Moon adrastea = new Moon("Adrastea", 1, 0, "GREY", 129, 1, jupiter);
+        Moon amalthea = new Moon("Amalthea", 1, 0, "GREY", 181, 1, jupiter);
+        Moon thebe = new Moon("Thebe", 1, 0, "GREY", 222, 1, jupiter);
         Moon Io = new Moon("Io", 1, 0, "GREY", 422, 28126, jupiter);
         Moon Europa = new Moon("Europa", 1, 0, "GREY", 671, 20149, jupiter);
         Moon Ganymede = new Moon("Ganymede", 1, 0, "GREY", 1070, 42186, jupiter);
@@ -37,18 +37,18 @@ class Astronomy
         Moon Himalia = new Moon("Himalia", 1, 0, "GREY", 11480, 250, jupiter);
         Moon Lysithea = new Moon("Lysithea", 1, 0, "GREY", 11720, 259, jupiter);
         Moon Elara = new Moon("Elara", 1, 0, "GREY", 11737, 259, jupiter);
-        Moon Ananke = new Moon("Ananke", 1, 0, "GREY", 21200, -631.00, jupiter);
-        Moon Carme = new Moon("Carme", 1, 0, "GREY", 22600, -692.00, jupiter);
-        Moon Pasiphae = new Moon("Pasiphae", 1, 0, "GREY", 23500, -735.00, jupiter);
-        Moon Sinope = new Moon("Sinope", 1, 0, "GREY", 23700, -758.00, jupiter);
+        Moon Ananke = new Moon("Ananke", 1, 0, "GREY", 21200, -631, jupiter);
+        Moon Carme = new Moon("Carme", 1, 0, "GREY", 22600, -692, jupiter);
+        Moon Pasiphae = new Moon("Pasiphae", 1, 0, "GREY", 23500, -735, jupiter);
+        Moon Sinope = new Moon("Sinope", 1, 0, "GREY", 23700, -758, jupiter);
 
-        Moon Pan = new Moon("Pan", 1, 0, "GREY", 134, 0, saturn);
-        Moon Atlas = new Moon("Atlas", 1, 0, "GREY", 138, 0, saturn);
-        Moon Prometheus = new Moon("Prometheus", 1, 0, "GREY", 139, 0, saturn);
-        Moon Pandora = new Moon("Pandora", 1, 0, "GREY", 142, 0, saturn);
-        Moon Epimetheus = new Moon("Epimetheus", 1, 0, "GREY", 151, 0, saturn);
-        Moon Janus = new Moon("Janus", 1, 0, "GREY", 151, 0, saturn);
-        Moon Mimas = new Moon("Mimas", 1, 0, "GREY", 186, 0, saturn);
+        Moon Pan = new Moon("Pan", 1, 0, "GREY", 134, 1, saturn);
+        Moon Atlas = new Moon("Atlas", 1, 0, "GREY", 138, 1, saturn);
+        Moon Prometheus = new Moon("Prometheus", 1, 0, "GREY", 139, 1, saturn);
+        Moon Pandora = new Moon("Pandora", 1, 0, "GREY", 142, 1, saturn);
+        Moon Epimetheus = new Moon("Epimetheus", 1, 0, "GREY", 151, 1, saturn);
+        Moon Janus = new Moon("Janus", 1, 0, "GREY", 151, 1, saturn);
+        Moon Mimas = new Moon("Mimas", 1, 0, "GREY", 186, 1, saturn);
         Moon Enceladus = new Moon("Enceladus", 1, 0, "GREY", 238, 13516, saturn);
         Moon Tethys = new Moon("Moon", 1, 0, "GREY", 295, 32509, saturn);
         Moon Telesto = new Moon("Telesto", 1, 0, "GREY", 295, 32509, saturn);
@@ -61,16 +61,16 @@ class Astronomy
         Moon Iapetus = new Moon("Iapetus", 1, 0, "GREY", 3561, 79, saturn);
         Moon Phoebe = new Moon("Phoebe", 1, 0, "GREY", 12952, -550, saturn);
 
-        Moon Cordelia = new Moon("Cordelia", 1, 0, "GREY", 50, 0, uranus);
-        Moon Ophelia = new Moon("Ophelia", 1, 0, "GREY", 54, 0, uranus);
-        Moon Bianca = new Moon("Bianca", 1, 0, "GREY", 59, 0, uranus);
-        Moon Cressida = new Moon("Cressida", 1, 0, "GREY", 62, 0, uranus);
-        Moon Desdemona = new Moon("Desdemona", 1, 0, "GREY", 63, 0, uranus);
-        Moon Juliet = new Moon("Juliet", 1, 0, "GREY", 64, 0, uranus);
-        Moon Portia = new Moon("Portia", 1, 0, "GREY", 66, 0, uranus);
-        Moon Rosalind = new Moon("Rosalind", 1, 0, "GREY", 70, 0, uranus);
-        Moon Belinda = new Moon("Belinda", 1, 0, "GREY", 75, 0, uranus);
-        Moon Puck = new Moon("Puck", 1, 0, "GREY", 86, 0, uranus);
+        Moon Cordelia = new Moon("Cordelia", 1, 0, "GREY", 50, 1, uranus);
+        Moon Ophelia = new Moon("Ophelia", 1, 0, "GREY", 54, 1, uranus);
+        Moon Bianca = new Moon("Bianca", 1, 0, "GREY", 59, 1, uranus);
+        Moon Cressida = new Moon("Cressida", 1, 0, "GREY", 62, 1, uranus);
+        Moon Desdemona = new Moon("Desdemona", 1, 0, "GREY", 63, 1, uranus);
+        Moon Juliet = new Moon("Juliet", 1, 0, "GREY", 64, 1, uranus);
+        Moon Portia = new Moon("Portia", 1, 0, "GREY", 66, 1, uranus);
+        Moon Rosalind = new Moon("Rosalind", 1, 0, "GREY", 70, 1, uranus);
+        Moon Belinda = new Moon("Belinda", 1, 0, "GREY", 75, 1, uranus);
+        Moon Puck = new Moon("Puck", 1, 0, "GREY", 86, 1, uranus);
         Moon Miranda = new Moon("Miranda", 1, 0, "GREY", 130, 14977, uranus);
         Moon Ariel = new Moon("Ariel", 1, 0, "GREY", 191, 19025, uranus);
         Moon Umbriel = new Moon("Umbriel", 1, 0, "GREY", 266, 41730, uranus);
@@ -82,11 +82,11 @@ class Astronomy
         Moon Prospero = new Moon("Prospero", 1, 0, "GREY", 16568, -2019, uranus);
         Moon Setebos = new Moon("Setebos", 1, 0, "GREY", 17681, -2239, uranus);
 
-        Moon Naiad = new Moon("Naiad", 1, 0, "GREY", 48, 0, neptune);
-        Moon Thalassa = new Moon("Thalassa", 1, 0, "GREY", 50, 0, neptune);
-        Moon Despina = new Moon("Despina", 1, 0, "GREY", 53, 0, neptune);
-        Moon Galatea = new Moon("Galatea", 1, 0, "GREY", 62, 0, neptune);
-        Moon Larissa = new Moon("Larissa", 1, 0, "GREY", 74, 0, neptune);
+        Moon Naiad = new Moon("Naiad", 1, 0, "GREY", 48, 1, neptune);
+        Moon Thalassa = new Moon("Thalassa", 1, 0, "GREY", 50, 1, neptune);
+        Moon Despina = new Moon("Despina", 1, 0, "GREY", 53, 1, neptune);
+        Moon Galatea = new Moon("Galatea", 1, 0, "GREY", 62, 1, neptune);
+        Moon Larissa = new Moon("Larissa", 1, 0, "GREY", 74, 1, neptune);
         Moon Triton = new Moon("Triton", 1, 0, "GREY", 355, -5, neptune);
         Moon Nereid = new Moon("Nereid", 1, 0, "GREY", 5513, 360, neptune);
 
@@ -94,29 +94,35 @@ class Astronomy
         Moon Nix = new Moon("Nix", 1, 0, "GREY", 49, 24, pluto);
         Moon Hydra = new Moon("Nereid", 1, 0, "GREY", 65, 38, pluto);
 
-        List<SpaceObject> allPlanets = new List<SpaceObject>{mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto};
-        List<Moon> allMoons = new List<Moon>{moon, phobis, Deimos, metis, adrastea, amalthea, thebe, Io, Europa, Ganymede, Callisto, Leda, Himalia, Lysithea, Elara, Ananke, Carme, Pasiphae, Sinope, Pan, Atlas, Prometheus, Pandora, Epimetheus, Janus, Mimas, Enceladus, Tethys, Telesto, Calypso, Dione, Helene, Rhea, Titan, Hyperion, Iapetus, Phoebe, Cordelia, Bianca, Cressida, Desdemona, Juliet, Portia, Rosalind, Belinda, Puck, Miranda, Ariel, Umbriel, Titania, Oberon, Caliban, Stephano, Sycorax, Prospero, Setebos, Naiad, Thalassa, Despina, Galatea, Larissa, Triton, Nereid, Charon, Nix, Hydra};
+        List<SpaceObject> allPlanets = new List<SpaceObject> { mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto };
+        List<Moon> allMoons = new List<Moon> { moon, phobis, Deimos, metis, adrastea, amalthea, thebe, Io, Europa, Ganymede, Callisto, Leda, Himalia, Lysithea, Elara, Ananke, Carme, Pasiphae, Sinope, Pan, Atlas, Prometheus, Pandora, Epimetheus, Janus, Mimas, Enceladus, Tethys, Telesto, Calypso, Dione, Helene, Rhea, Titan, Hyperion, Iapetus, Phoebe, Cordelia, Bianca, Cressida, Desdemona, Juliet, Portia, Rosalind, Belinda, Puck, Miranda, Ariel, Umbriel, Titania, Oberon, Caliban, Stephano, Sycorax, Prospero, Setebos, Naiad, Thalassa, Despina, Galatea, Larissa, Triton, Nereid, Charon, Nix, Hydra };
 
 
         Sun.satellite = allPlanets;
-        foreach (SpaceObject p in allPlanets)
+        foreach (Planet p in allPlanets)
         {
             foreach (Moon m in allMoons)
             {
-                if(m.orbiting == p)
+                if (m.orbiting == p)
                 {
-                    p.satellite.add(m);
+                    p.satellite.Add(m);
+                }
+                else if (m.orbiting == pluto)
+                {
+                    pluto.satellite.Add(m);
                 }
             }
         }
 
 
+        Sun.Draw();
 
-
+        /*
         foreach (SpaceObject obj in solarSystem)
         {
             obj.Draw();
         }
+        */
         Console.ReadLine();
     }
 }
--- Assignments/Assignment3/SpaceObject.cs
+++ Assignments/Assignment3/SpaceObject.cs
@@ -1,23 +1,25 @@
 using System;
+using System.Collections.Generic;
 /*
 Use coordinates of circle to find out coordinates of planets and moons in orbit
 */
 
 namespace SpaceSim
 {
-    public static int OSC = 365; // The Orbital speed calculator value to find out how fast an object is supposed to spin around it's "Parent"
+
     public class SpaceObject
     {
-        protected String name { get; set; }// Name of object
-        protected int radius { get; set; } // Radius of object
-        protected int x { get; set; } // X coordinates
-        protected int y { get; set; } // Y coordinates
-        public SpaceObject(String _name, int _radius, int _x, int _y)
+        public static int OSC = 365; // The Orbital speed calculator value to find out how fast an object is supposed to spin around it's "Parent"
+        public String name { get; protected set; }// Name of object
+        public int radius { get; protected set; } // Radius of object
+        public int x { get; protected set; } // X coordinates
+        public int y { get; protected set; } // Y coordinates
+        public SpaceObject(String _name, int _radius)
         {
             name = _name;
             radius = _radius;
-            x = _x;
-            y = _y;
+            x = 0;
+            y = 0;
         }
         public virtual void Draw()
         {
@@ -28,15 +30,14 @@ namespace SpaceSim
 
     public class Star : SpaceObject
     {
-        protected int rotPeriod { get; set; } // Rotational period (length of day)
-        protected String color { get; set; } // Color of object
-        protected List<SpaceObject> satellite { get; set; } // List of satellites, like comets, planets(Moons for planet) and so on
-        public Star(String _name, int _radius, int _rotPeriod, String _color, List<SpaceObject> satellite) : base(_name, _radius)
+        public int rotPeriod { get; protected set; } // Rotational period (length of day)
+        public String color { get; protected set; } // Color of object
+        public List<SpaceObject> satellite { get; set; } // List of satellites, like comets, planets(Moons for planet) and so on
+        public Star(String _name, int _radius, int _rotPeriod, String _color) : base(_name, _radius)
         {
             radius = _radius;
             rotPeriod = _rotPeriod;
             color = _color;
-            satellite = _satellite;
             x = 0;
             y = 0;
         }
@@ -47,25 +48,29 @@ namespace SpaceSim
             Console.WriteLine("Planets: ");
             foreach (SpaceObject item in satellite)
             {
-                Console.WriteLine("   *" + satellite.name);
+                Console.WriteLine("   *" + item.name);
             }
         }
     }
 
-    public class Planet : Sun
+    public class Planet : Star
     {
-        protected SpaceObject orbiting { get; set; } // What object this object is orbiting
-        protected int orbPeriod; // Orbital period (Length of year) TODO: Implement
-        protected int orbRadius { get; set; } // Radius of orbit
-        protected int orbSpeed { get; set; } // Speed of orbit
+        public SpaceObject orbiting { get; protected set; } // What object this object is orbiting
+        public int orbPeriod; // Orbital period (Length of year) TODO: Implement
+        public int orbRadius { get; protected set; } // Radius of orbit
+        public int orbSpeed { get; protected set; } // Speed of orbit
+        public new List<SpaceObject> satellite { get; set; }
 
-        public Planet(String _name, int _radius, int _rotPeriod, String _color, int _orbRadius, int _orbPeriod, SpaceObject _orbiting, List<SpaceObject> _satellite) : base(_name, _radius, _rotPeriod, _color, _satellite)
+        public Planet(String _name, int _radius, int _rotPeriod, String _color, int _orbRadius, int _orbPeriod, SpaceObject _orbiting) : base(_name, _radius, _rotPeriod, _color)
         {
             orbiting = _orbiting;
             orbRadius = _orbRadius;
+            orbPeriod = _orbPeriod;
             x = _orbRadius;
-            orbiting = _orbiting;
-            orbSpeed = OSC/orbPeriod;
+            y = 0;
+            orbSpeed = OSC / orbPeriod;
+            satellite = new List<SpaceObject>();
+
         }
 
         public virtual void position(int time)
@@ -80,35 +85,63 @@ namespace SpaceSim
             Console.WriteLine("Moons: ");
             foreach (SpaceObject item in satellite)
             {
-                Console.WriteLine("   *" + satellite.name);
+                Console.WriteLine("   *" + item.name);
             }
         }
     }
 
     public class Moon : Planet
     {
-        public Moon(String _name, int _radius, int _rotPeriod, String _color, int _orbRadius, int _orbPeriod, SpaceObject _orbiting) : base(_name, _radius, _rotPeriod, _color, _satellite, _orbPeriod, _orbiting) { }
+        public Moon(String _name,
+        int _radius,
+        int _rotPeriod,
+        String _color,
+        int _orbRadius,
+        int _orbPeriod,
+        SpaceObject _orbiting) 
+        : base(_name, 
+        _radius, 
+        _rotPeriod, 
+        _color, 
+        _orbRadius,
+        _orbPeriod,
+        _orbiting) 
+        { 
+        }
         public override void Draw()
         {
             Console.Write("Moon: ");
             base.Draw();
         }
     }
-
+/*
     public class Comet : SpaceObject
     {
-        public Comet(String _name, int _radius, int _rotPeriod, String _color) : base(_name, _radius, _rotPeriod, _color) { }
+        public Comet(String _name,
+        int _radius,
+        int _rotPeriod,
+        String _color,
+        int _orbRadius,
+        int _orbPeriod,
+        SpaceObject _orbiting) 
+        : base(_name, 
+        _radius, 
+        _rotPeriod, 
+        _color, 
+        _orbRadius,
+        _orbPeriod,
+        _orbiting) { }
         public override void Draw()
         {
             Console.Write("Comet: ");
             base.Draw();
         }
     }
-
+*/
     public class Asteroid : Planet
     {
         protected int number { get; set; } // Asteroids are identified with a number before their name
-        public Asteroid(String _name, int _radius, int _rotPeriod, String _color, int _orbRadius, int _orbPeriod, SpaceObject _orbiting, int _number) : base(_name, _radius, _rotPeriod, _color, _satellite, _orbSpeed, _orbiting)
+        public Asteroid(String _name, int _radius, int _rotPeriod, String _color, int _orbRadius, int _orbPeriod, SpaceObject _orbiting, int _number) : base(_name, _radius, _rotPeriod, _color, _orbRadius, _orbPeriod,_orbiting)
         {
             number = _number;
         }
@@ -120,19 +153,38 @@ namespace SpaceSim
         }
     }
 
-    public class AsteroidBelt : Sun
+    /*
+    public class AsteroidBelt : Planet
     {
-        public AsteroidBelt(String _name, SpaceObject _orbiting) : base(_name, _orbiting) { }
+        public AsteroidBelt(String _name, SpaceObject _orbiting) : base(_name, _radius, _rotPeriod, _color, _orbiting) { }
         public override void Draw()
         {
             Console.Write("Asteroid belt: ");
             base.Draw();
         }
     }
-
+    */
     public class DwarfPlanet : Planet
     {
-        public DwarfPlanet(String _name, int _radius, int _rotPeriod, String _color, int _orbRadius, int _orbSpeed, SpaceObject _orbiting) : base(_name, _radius, _rotPeriod, _color, _satellite, _orbSpeed, _orbiting) { }
+        public new List<SpaceObject> satellite { get; set; }
+        
+        public DwarfPlanet(String _name,
+        int _radius,
+        int _rotPeriod,
+        String _color,
+        int _orbRadius,
+        int _orbPeriod,
+        SpaceObject _orbiting) 
+        : base(_name, 
+        _radius, 
+        _rotPeriod, 
+        _color, 
+        _orbRadius,
+        _orbPeriod,
+        _orbiting) 
+        {
+            satellite = new List<SpaceObject>();
+        }
 
         public override void Draw()
         {
